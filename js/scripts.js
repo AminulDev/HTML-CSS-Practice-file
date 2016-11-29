0fd9826b465b6f2c768193015c20ec4aa0b68936
aminuldev <aminuldev1@gmail.com>
2016-11-29 22:31:06 +0600

    add image, css file

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/* --------------------------------------------------------
+Style Sheet for Website's Name :
+version: 1.0
+last modified:
+email: aminuldev1@gmail.com
+website:
+author:
+author URL: aminuldev.com
+----------------------------------------------------------*/
